68cd7c03e351db204b0651fc902888a1a8f15378
Derevyankin Denis <derevyankindenis@guthub.com>
2022-01-19 20:03:42 +0300

    restore replaceURLs.test.js

---
+++ __tests__/replaceURLs.test.js
@@ -0,0 +1,27 @@
+import fs from 'fs/promises';
+// eslint-disable-next-line
+import {
+  getFixturePath,
+  encode,
+} from './testUtils';
+import replaceURLs from '../src/replaceURLs';
+
+const HTML_BEFORE_PATH = getFixturePath('site-com-blog-about.html');
+const HTML_AFTER_PATH = getFixturePath('expected/site-com-blog-about.html');
+
+const REPLACES = {
+  '/blog/about/assets/styles.css': 'site-com-blog-about_files/site-com-blog-about-assets-styles.css',
+  '/blog/about': 'site-com-blog-about_files/site-com-blog-about.html',
+  '/photos/me.jpg': 'site-com-blog-about_files/site-com-photos-me.jpg',
+  '/photos/me2.jpg': 'site-com-blog-about_files/site-com-photos-me2.jpg',
+  'https://site.com/assets/scripts.js': 'site-com-blog-about_files/site-com-assets-scripts.js',
+};
+
+const replacer = (url) => REPLACES[url];
+
+test('replaceURLs', async () => {
+  const HTMLBefore = await fs.readFile(HTML_BEFORE_PATH, encode);
+  const expectedHTML = await fs.readFile(HTML_AFTER_PATH, encode);
+  const resultHTML = replaceURLs(HTMLBefore, replacer);
+  expect(resultHTML.trim()).toEqual(expectedHTML.trim());
+});
